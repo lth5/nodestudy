bdc73b5dee9291156d76b9f6d88afaba2d51542b
liuteng <liuteng@xin.com>
2018-05-07 18:49:24 +0800

    初建项目

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path')
+// const express = require('express')
+// const app = express()
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
+
+// app.set('views', path.join(__dirname, 'views'))// 设置存放模板文件的目录
+// app.set('view engine', 'ejs')// 设置模板引擎为 ejs
+
+// app.use('/', indexRouter)
+// app.use('/users', userRouter)
+
+// app.listen(3000)
+
+
+const express = require('express')
+const app = express()
+app.set('views', path.join(__dirname, 'views'))// 设置存放模板文件的目录
+app.set('view engine', 'ejs')// 设置模板引擎为 ejs
+
+app.use('/', indexRouter)
+app.use('/users', userRouter)
+app.use(function (req, res, next) {
+  console.log('1')
+  next()
+})
+
+app.use(function (req, res, next) {
+  console.log('2')
+  res.status(200).end()
+})
+
+
+
+app.listen(3000)
