55d85536bead4f0b613f471c30b7dca3adfbb214
liuteng <liuteng@xin.com>
2019-05-22 10:17:04 +0800

    temp

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ app.use('/', indexRouter)
 app.engine('html',ejs.__express);
 app.set('view engine', 'html');
 const server = app.listen(3000,()=>{
-	
-	
-	
-	
-	
     var host = server.address().address
     var port = server.address().port
     console.log('服务启动 http://%s:%s', host, port);
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
+  res.send('sdsdsdsd, ' + req.params.name)
 })
 
 module.exports = router
