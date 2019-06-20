568bfb67ceae4996bc52905e7b3e796afb4c246f
liuteng <liuteng@xin.com>
2019-06-20 19:05:51 +0800

    temp

--- routes/kuayu.js
+++ routes/kuayu.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/cross', function (req, res) {
+const whiteList = {
+	"http://localhost:3000":true,
+	"http://localhost:4000":true,
+	"http://localhost:8080":true,
+}
+	
+router.all('/cross', function (req, res) {
 	console.log(req.headers.origin)
-	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+	if (whiteList[req.headers.origin]) {
+		console.log(req.headers.origin)
+		res.header("Access-Control-Allow-Origin",req.headers.origin );
+	}
+	
 	res.header('Access-Control-Allow-Headers', 'Content-type');
-	res.header("Access-Control-Allow-Methods", "GET");
+	res.header("Access-Control-Allow-Methods", "PUT,GET");
 	res.send('cross跨域牵手成功')
 })
 router.get('/jsonp/:index', function (req, res) {
-	var _callback = req.query.callback;
+	var _callback = req.query.cb;
 	var _data = { email: 'example@163.com', name: 'jaxu' ,index:req.params.index};
+	console.log(req.headers)
+	console.log()
 	if (_callback){
 		res.type('text/javascript');
 		res.send(_callback + '(' + JSON.stringify(_data) + ')');
